ae03eab99e03c844c7ebc89df44010e0d8257d8a
Magaiver007 <58440856+magaiver007@users.noreply.github.com>
2022-04-10 20:26:46 +0300

    ver1

--- carspeed.py
+++ carspeed.py
@@ -1,29 +1,34 @@
 # initialize class car (brand,color,year,speed,orientation)
 # in case args speed and orientation are null they get default values
 class car:
-    def __init__(self, brand, color, year, speed=0, orientation="straight"):
+    def __init__(self, brand, color, year):
         self.brand = brand
         self.color = color
         self.year = year
-        self.speed = speed
-        self.orientation = orientation
-
-    # function speed_up gets arg int
-    def speed_up(self, speed):
-        self.speed += speed
-        return self.speed
+        self.speed = int()
+        self.orientation = ""
 
     # function turn gets arg str
     def turn(self, orientation):
         self.orientation = orientation
         return self.orientation
 
+    # function speed_up gets arg int
+    def speed_up(self, speed):
+        self.speed += speed
+        return self.speed
 
-convertible = car("BMW", "black", 2010)
-print(convertible.brand, convertible.color, convertible.year, convertible.speed, convertible.orientation)
-convertible.turn("right")
-print(convertible.brand, convertible.color, convertible.year, convertible.speed, convertible.orientation)
-sedan = car("Nissan", "red", year=2019)
+
+convertible = car("BMW", "μαύρο", 2010)
+print(convertible.brand, convertible.color, convertible.year, convertible.orientation)
+
+convertible.turn("δεξιά")
+print(convertible.brand, convertible.color, convertible.year, convertible.orientation)
+
+sedan = car("Nissan", "κόκκινο", year=2019)
 print(sedan.brand, sedan.color, sedan.year, sedan.speed, sedan.orientation)
+
 sedan.speed_up(speed=90)
 print(sedan.brand, sedan.color, sedan.year, sedan.speed, sedan.orientation)
+
+
